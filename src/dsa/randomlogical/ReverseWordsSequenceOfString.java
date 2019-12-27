2eec7ac10d3c501786b8a83c63ec92693546f255
Akash Mulik <akashmulik@users.noreply.github.com>
2019-12-27 18:59:38 +0530

    Update ReverseWordsSequenceOfString.java

--- src/dsa/randomlogical/ReverseWordsSequenceOfString.java
+++ src/dsa/randomlogical/ReverseWordsSequenceOfString.java
@@ -4,28 +4,39 @@ public class ReverseWordsSequenceOfString {
 
 	public static void main(String[] args) {
 
-		reverseWords("I love Java");
+		reverseWords("Me love Java");
 	}
 
 	static void reverseWords(String s) {
 
-		String[] res = new String[s.length()];
-		int arrCnt = 0;
 		StringBuffer word = new StringBuffer();
 
 		for (int i = 0; i < s.length(); i++) {
 
 			if (s.charAt(i) != ' ') {
 				word.append(s.charAt(i));
-			} 
+			}
 			if(s.charAt(i) == ' ' || i == s.length()-1) {
-				res[arrCnt] = word.toString();
-				arrCnt++;
+				System.out.print(word.toString()+ " ");
 				word = new StringBuffer();
 			}
 		}
-		for(String re : res)
-			System.out.println(re);
+		//second logic
+		System.out.println("\n"+"----------");
+		for(int i = s.length()-1; i >=0; i--) {
+			int end = i, j = i;
+			while(j > 0 && s.charAt(j-1) != ' ') {
+				j--;
+				end--;
+			}
+			while(j <= i) {
+				System.out.print(s.charAt(j));
+				j++;
+			}
+			if(end !=0 && s.charAt(end-1) == ' ')
+			System.out.print(" ");
+			i = end-1;
+		}
 	}
 
 }
